9e51176611ad8cbaff4f369e43c7357480cfc731
Krishnamurti Pandaram <krishna916@outlook.com>
2024-12-30 09:48:58 +0530

    Initial commit

---
+++ 02variables/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "krishna916"
+	fmt.Println(username)
+	fmt.Printf("variable is of type: %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("variable is of type: %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("variable is of type: %T \n", smallVal)
+
+	var smallFloat float32 = 255.455344343433453454
+	fmt.Println(smallFloat)
+	fmt.Printf("variable is of type: %T \n", smallFloat)
+
+	// default values and some aliases
+}
